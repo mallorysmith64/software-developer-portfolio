80c5f5f7f13fa67df385f62f17e021771110f492
Mars <mallorysmith6464@gmail.com>
2019-09-03 15:59:37 -0400

    resized photos

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,35 +1,54 @@
 import React, { Component } from 'react'
-import SpaceX from './images/space_x_screenshot.png'
-import Octodex from './images/octodex_screenshot.png'
+// import SpaceX from './images/space_x_screenshot.png'
+import Octodex from './images/octodex_screenshot_resized.png'
 import Minesweeper from './images/minesweeper_screenshot.png'
-import TV from './images/tv_guide_screenshot.png'
+import TV from './images/tv_guide_screenshot_resized.png'
 import Weather from './images/weather_screenshot.png'
 
 class MyWork extends Component {
   render() {
     return (
       <>
-        <main className="projects">
-          <section className="space-x-container">
+        <section className="project-header">
+          <h2>My Work</h2>
+        </section>
+        <main className="photo-container">
+          {/* <section className="space-x-container">
             <h1 className="space-x">SpaceX Project</h1>
             <img src={SpaceX} alt="space-x project overview" />
+          </section> */}
+          <section className="img-style">
+            <img
+              src={Octodex}
+              alt="octodex project overview"
+              height="400"
+              width="300"
+            />
           </section>
           <section>
-            <h1 className="octodex-container">Octodex Project</h1>
-            <img src={Octodex} alt="octodex project overview" />
+            <img
+              src={Minesweeper}
+              alt="minesweeper project overview"
+              height="400"
+              width="300"
+            />
           </section>
           <section>
-            <h1 className="minesweeper-container">Minesweeper</h1>
-            <img src={Minesweeper} alt="minesweeper project overview" />
+            <img
+              src={TV}
+              alt="TV guide project overview"
+              height="400"
+              width="300"
+            />
           </section>
-          <section>
-            <h1 className="tv-guide-container">TV Guide</h1>
-            <img src={TV} alt="TV guide project overview" />
-          </section>
-          <section>
-            <h1 className="weather-container">Weather</h1>
-            <img src={Weather} alt="weather project overview" />
-          </section>
+          {/* <section>
+            <img
+              src={Weather}
+              alt="weather project overview"
+              height="500"
+              width="400"
+            />
+          </section> */}
         </main>
       </>
     )
