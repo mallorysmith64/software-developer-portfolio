c36191a51df5197c10408f8e7d127d3cc92136ee
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-13 17:11:46 -0500

    added to about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,46 +5,34 @@ class About extends Component {
   render() {
     return (
       <>
-        <section className="about-me">
-          <img
-            className="img-style"
-            src={ProfilePic}
-            alt="Mallory Smith's profile"
-            height="200"
-            width="200"
-          />
-          <section className="about-container">
-            <header className="about">
-              <h5>Who I Am</h5>
-            </header>
-
-            <p>
-            I am an alumni from a fully immersive coding bootcamp called Suncoast Developer Guild.
-            </p>
+      <div className="about-me">
+        <img
+          className="img-style"
+          src={ProfilePic}
+          alt="Mallory Smith's profile"
+          height="200"
+          width="200"
+        />
+        <section className="about-container">
+            <h5>Who I Am</h5>
+            <p>I am a passionate web developer and an alumni from a fully immersive coding bootcamp called Suncoast Developer Guild.</p>
 
             <h5>What Motivates Me</h5>
-            <p>
-              What motivates me the most is the satisfaction that comes with
-              problem solving and creating my own projects. Development is so much fun!
-            </p>
+            <p>What motivates me the most is the satisfaction that comes with
+              problem solving and creating my own projects. Development is so much fun!</p>
 
             <h5>Where I Want To Go</h5>
-            <p>
-              Short-Term Goals:
-            - I would like to earn my AWS associate developer certification. 
-            - I want to increase my ability to code in C#, javascript, and python.
-            </p>
-            <p>
-              Long-Term Goals:
-            - 5 years from now, I want to be able to teach others my skill set.
-            </p>
+            <p>1) I am currently working on earning my AWS associate developer certification. </p>
+            <p>2) I want to build more projects in C# and javascript.</p>
+            <p>3) I am currently learning python and would like to start building in that language, as well.</p>
+            <p>4) 5 years from now, I want to be able to teach others my skill set.</p>
 
-            <section>
+            <div>
               <a href='./resume/MalloryASmith_Resume.pdf' className="waves-effect waves-light btn" download>Download Resume</a>
-            </section>
+            </div>
 
           </section>
-        </section>
+        </div>
       </>
     )
   }
