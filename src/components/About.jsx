afc13ecfeded2b2d7cd08af1356eeea2522b9442
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-13 16:55:16 -0500

    updated about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,19 +17,26 @@ class About extends Component {
             <header className="about">
               <h5>Who I Am</h5>
             </header>
+
             <p>
-              When I am not coding, I am studying biology or practicing doing
-              yoga.
+            I am an alumni from a fully immersive coding bootcamp called Suncoast Developer Guild.
             </p>
+
             <h5>What Motivates Me</h5>
             <p>
               What motivates me the most is the satisfaction that comes with
-              problem solving and creating my own projects.
+              problem solving and creating my own projects. Development is so much fun!
             </p>
+
             <h5>Where I Want To Go</h5>
             <p>
-              5 years from now I would like to be able to combine some of my
-              biology knowledge with my coding skills.
+              Short-Term Goals:
+            - I would like to earn my AWS associate developer certification. 
+            - I want to increase my ability to code in C#, javascript, and python.
+            </p>
+            <p>
+              Long-Term Goals:
+            - 5 years from now, I want to be able to teach others my skill set.
             </p>
 
             <section>
