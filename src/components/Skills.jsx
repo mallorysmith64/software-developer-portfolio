8d3f88df6e804d2281a56cf86580941764be8343
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-15 01:22:17 -0500

    divided up skill section into multiple categories, changed css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,54 +4,64 @@ class About extends Component {
   render() {
     return (
       <>
-        <header className="skill-header">
-            <h2>Technical Skills</h2>
-        </header>
       <div className="skill-container">
-      <section className="frontend-column">
-            <ul className="frontend-skills">
-            <h5>Front-End</h5>
-            <li>React.js</li>
+
+      <section className="language-container">
+        <ul className="languages">
+          <h5>Languages & Databases</h5>
+            <li>C#</li>
             <li>Javascript</li>
-            <li>Bulma (CSS Framework)</li>
-            <li>Materialize CSS</li>
+            <li>Python</li>
+            <li>PostgreSQL</li>
+            <li>SQL</li>
+            <li>SCSS/SASS</li>
             <li>CSS Flexbox</li>
             <li>HTML5</li>
-       </ul>
+        </ul>
       </section>
 
-    <section className="backend-column">
-        <ul className="backend-skills">
-            <h5>Back-End</h5>
-            <li>C#</li>
-            <li>.NET Core</li>
-            <li>Entity Framework</li>
-            <li>SQL</li>
-            <li>PostgreSQL</li>
+      <section className="framework-and-library-container">
+        <ul className="frameworks-and-libraries">
+          <h5>Frameworks & Libraries</h5>
+           <li>React.js</li>
+           <li>.NET Core</li>
+           <li>Entity Framework</li>
+           <li></li>
+        </ul>
+      </section>
+
+      <section className="concept-container">
+        <ul className="concepts">
+          <h5>Programming Concepts</h5>
+          <li>Object Oriented Programming (OOP)</li>
+          <li>Model View Controller (MVC)</li>
+          <li>RESTful API</li>
+          <li>Mobile First Design</li>
+          <li>Single Page Apps (SPAs)</li>
+        </ul>
+      </section>
+
+      <section className="software-tool-container">
+        <ul className="software-tools">
+          <h5>Software Tools</h5>
+          <li>Visual Studios Code (VS Code)</li>
+          <li>Slack</li>
+          <li>Trello</li>
+          <li>Git</li>
+          <li>GitHub</li>
+        </ul>
+      </section>
+
+      <section className="deployment-environment-container">
+        <ul className="deployment-environments">
+          <h5>Deployment Environments</h5>
             <li>Docker</li>
-            <li>Model-View-Controller (MVC)</li>
-        </ul>
-    </section>
-
-    <section className="aws-column">
-        <ul className="aws-skills">
-            <h5>AWS</h5>
-            <li>IAM (Identity and Access Management)</li>
-            <li>S3</li>
-        </ul>
-    </section>
-
-    <section className="other-skills-column">
-        <ul className="other-skills">
-            <h5>Other</h5>
-            <li>Git</li>
-            <li>Swagger API</li>
-            <li>Heroku</li>
+            <li>AWS</li>
             <li>Netlify</li>
-            <li>RESTful API</li>
+            <li>Heroku</li>
         </ul>
-    </section>
-  </div>
+      </section>
+    </div>
       </>
     )
   }
