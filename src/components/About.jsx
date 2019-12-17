061eb2b749c0b5a89a18da709cd0036af298c31b
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-17 00:35:52 -0500

    made about me section easier to read by adding padding and line height

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import ProfilePic from './images/mallory_profile_picture.jpeg'
+import Resume from './resume/MalloryASmith_Resume.pdf'
 
 class About extends Component {
   render() {
     return (
       <>
       <div className="about-me">
+        <section className="profile-pic-container">
         <img
           className="img-style"
           src={ProfilePic}
@@ -13,19 +15,21 @@ class About extends Component {
           height="200"
           width="200"
         />
+        </section>
+
         <section className="about-container">
             <h5>Who I Am</h5>
             <p>I am a passionate web developer and an alumni from a fully immersive coding bootcamp called Suncoast Developer Guild.</p>
 
             <h5>What Motivates Me</h5>
             <p>What motivates me the most is the satisfaction that comes with
-              problem solving and creating my own projects. Development is so much fun!</p>
+              problem solving and creating projects.</p>
 
             <h5>Where I Want To Go</h5>
             <p>1) I am currently working on earning my AWS associate developer certification. </p>
             <p>2) I want to build more projects in C# and javascript.</p>
             <p>3) I am currently learning python and would like to start building in that language, as well.</p>
-            <p>4) 5 years from now, I want to be able to teach others my skill set.</p>
+            <p>4) 5 years from now, I want to be able to teach others what I have learned.</p>
 
             <div>
               <a href='./resume/MalloryASmith_Resume.pdf' className="waves-effect waves-light btn" download>Download Resume</a>
