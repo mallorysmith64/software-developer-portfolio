49d6b0285be0e5f6f96c19c46334168343d8c1c6
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-11 12:18:35 -0500

    updated minesweeper url

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -236,7 +236,7 @@ class Projects extends Component {
 
               <div className="card-action">
                 <a
-                  href="https://69247e3e87a4260d9bf4efc2--minesweeper-2.netlify.app/"
+                  href="https://minesweeper-2.netlify.app/"
                   className="waves-effect waves-light btn"
                 >
                   Live
