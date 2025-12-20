4a1b03f31f041745c43f55e3b0c8cd821394d761
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-20 11:01:54 -0500

    fixed allowFullScreen and frameBorder errors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,9 +16,9 @@ class About extends Component {
               height="300"
               src="https://www.youtube.com/embed/meTAfu3g7Pc"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </section>
 
